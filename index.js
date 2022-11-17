985fc38864cabb81d67844d4a24930f576582e12
anthony-univ <gascaanthony0409@icloud.com>
2022-11-17 16:43:46 +0100

    add socket disconnect logout

--- index.js
+++ index.js
@@ -224,7 +224,55 @@ io.on('connection', function (socket) {
         }
     });
 
+
+		 // fermeture
+		 socket.on("disconnect", (player)=> { 
+			// si client était identifié (devrait toujours être le cas)
+			if (currentID) {
+				let player = {roomId: null, username: currentID}
+			
+				rooms.forEach(room => {
+					console.log("room" + room.players)
+					room.players.forEach(player2 => {
+						if(player2.username === currentID) {
+							console.log("Sortie de l'utilisateur " + currentID);
+							player.roomId =  room.id;
+						}
+					});
+				});
+				
+				console.log(player);
+				let room = rooms.find(r => r.id === player.roomId);
+				console.log(room);
+				
+				if(room !== undefined) { //aprteint a room
+					supprimerPlayerRoom(player);
+					
+					if(rooms[player.roomId].placePrise !== 0) {
+						// envoi de l'information de déconnexion
+						socket.broadcast.emit("message", { from: null, to: null, text: currentID + " a quitté la partie", date: Date.now() } );
+						//envoie de la nouvelle liste à jour
+						io.in(room.id).emit("liste",room.players);
+					}else {
+						console.log("delete room -> ", player.roomId)
+						rooms = rooms.filter(r => r.id !== player.roomId);
+					}
+					
+					// emission pour donner la liste des rooms aux clients (maj nombre players room)
+					socket.broadcast.emit('list rooms',rooms);
+				}
+				
+				// suppression de l'entrée
+				supprimer(currentID);
 	
+				// désinscription du client
+				currentID = null;
+			}
+
+			console.log("Client déconnecté");
+		});
+	
+		
 });
 
 
